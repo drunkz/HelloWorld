d985f00f30e3077d4b90709aadff97dbddc79772
drunkz <drunkz@163.com>
2021-05-26 13:53:49 +0800

    asdf

--- server/db_server/db_server.cpp
+++ server/db_server/db_server.cpp
@@ -5,7 +5,7 @@ db_server::db_server() {
     std::cout << "start" << std::endl;
 }
 db_server::~db_server() {
-     std::cout << "end" << std::endl;
+    std::cout << "end" << std::endl;
 }
 } // namespace Server
 
@@ -13,5 +13,5 @@ int main(int argc, char **argv) {
     using namespace Server;
     using namespace std;
     server_base *db = new db_server;
-    delete(db);
+    delete db;
 }
--- server/engine/server_base.h
+++ server/engine/server_base.h
@@ -3,8 +3,8 @@
 
 #include <boost/log/core.hpp>
 #include <boost/log/expressions.hpp>
-#include <boost/log/trivial.hpp>
 #include <boost/log/sources/severity_logger.hpp>
+#include <boost/log/trivial.hpp>
 #include <boost/log/utility/setup/common_attributes.hpp>
 #include <boost/log/utility/setup/console.hpp>
 #include <boost/log/utility/setup/file.hpp>
