6f74b2907b738aa410b59dd77268d5612bd2cca7
drunkz <drunkz@163.com>
2021-05-28 21:08:56 +0800

    asdf

--- server/engine/server_base.cpp
+++ server/engine/server_base.cpp
@@ -1,25 +1,41 @@
 #include "server_base.h"
-#include <boost/log/sinks/sync_frontend.hpp>
-#include <boost/log/sinks/text_ostream_backend.hpp>
-#include <boost/log/sources/record_ostream.hpp>
-#include <boost/smart_ptr/make_shared_object.hpp>
-#include <boost/smart_ptr/shared_ptr.hpp>
-#include <fstream>
+
 namespace Server {
 server_base::server_base() {
-    boost::log::add_file_log(boost::log::keywords::file_name =
-                                 "/home/dkz/ttt/aaa/sample_%Y-%m-%d_%H-%M-%S.txt",
-                             boost::log::keywords::rotation_size = 10 << 20,
-                             boost::log::keywords::format        = "[%TimeStamp%]: %Message%");
-    boost::log::add_console_log();
-    boost::log::core::get()->set_filter(boost::log::trivial::severity >= boost::log::trivial::info);
-    boost::log::add_common_attributes();
-    HW_TRACE << "A trace severity messagea";
-    HW_DEBUG << "A debug severity message";
-    HW_INFO << "An informational severity message";
-    HW_WARN << "A warning severity message";
-    HW_ERROR << "An error severity messagea";
-    HW_DEBUG << "A fatal severity message";
+    // logging::add_file_log(
+    //     logging::keywords::file_name     = "/home/dkz/ttt/aaa/sample_%Y-%m-%d_%H-%M-%S.txt",
+    //     logging::keywords::rotation_size = 10 << 20,
+    //     logging::keywords::format =
+    //         (logging::expressions::stream
+    //          << logging::expressions::format_date_time<boost::posix_time::ptime>(
+    //                 "TimeStamp", "%Y-%m-%d %H:%M:%S")
+    //          << ": [" << logging::trivial::severity << "] " << logging::expressions::smessage));
+    // logging::add_console_log(
+    //     std::cout,
+    //     logging::keywords::format =
+    //         (logging::expressions::stream
+    //          << logging::expressions::format_date_time<boost::posix_time::ptime>(
+    //                 "TimeStamp", "%Y-%m-%d %H:%M:%S")
+    //          << ": [" << logging::trivial::severity << "] " << logging::expressions::smessage));
+    typedef sinks::synchronous_sink<sinks::text_ostream_backend> text_sink;
+    boost::shared_ptr<text_sink> sink = boost::make_shared<text_sink>();
+    sink->locked_backend()->add_stream(
+        boost::make_shared<std::ofstream>("/home/dkz/ttt/aaa/sample.log"));
+    logging::core::get()->add_sink(sink);
+    logging::core::get()->set_filter(logging::trivial::severity >= logging::trivial::info);
+    // logging::add_common_attributes();
+    // HW_TRACE << "A trace severity messagea";
+    // HW_DEBUG << "A debug severity message";
+    // HW_INFO << "An informational severity message";
+    // HW_WARN << "A warning severity message";
+    // HW_ERROR << "An error severity messagea";
+    // HW_DEBUG << "A fatal severity message";
+    src::logger lg;
+    BOOST_LOG(lg) << "aaaaa!";
+    BOOST_LOG(lg) << "aaaaa!";
+    BOOST_LOG(lg) << "aaaaa!";
+    BOOST_LOG(lg) << "aaaaa!";
+    BOOST_LOG(lg) << "aaaaa!";
     L = luaL_newstate();
     if (L == nullptr) {
     } else {
--- server/engine/server_base.h
+++ server/engine/server_base.h
@@ -1,22 +1,38 @@
 #ifndef SERVER_BASE_H_INCLUDED
 #define SERVER_BASE_H_INCLUDED
 
+#include <boost/date_time/posix_time/posix_time_types.hpp>
 #include <boost/log/core.hpp>
 #include <boost/log/expressions.hpp>
+#include <boost/log/sinks/sync_frontend.hpp>
+#include <boost/log/sinks/text_ostream_backend.hpp>
+#include <boost/log/sources/logger.hpp>
+#include <boost/log/sources/record_ostream.hpp>
 #include <boost/log/sources/severity_logger.hpp>
+#include <boost/log/support/date_time.hpp>
 #include <boost/log/trivial.hpp>
 #include <boost/log/utility/setup/common_attributes.hpp>
 #include <boost/log/utility/setup/console.hpp>
 #include <boost/log/utility/setup/file.hpp>
+#include <boost/smart_ptr/make_shared_object.hpp>
+#include <boost/smart_ptr/shared_ptr.hpp>
+#include <fstream>
 #include <iostream>
 #include <lua.hpp>
 
-#define HW_TRACE BOOST_LOG_SEV(lg, boost::log::trivial::trace)
-#define HW_DEBUG BOOST_LOG_SEV(lg, boost::log::trivial::debug)
-#define HW_INFO BOOST_LOG_SEV(lg, boost::log::trivial::info)
-#define HW_WARN BOOST_LOG_SEV(lg, boost::log::trivial::warning)
-#define HW_ERROR BOOST_LOG_SEV(lg, boost::log::trivial::error)
-#define HW_FATAL BOOST_LOG_SEV(lg, boost::log::trivial::fatal)
+namespace logging = boost::log;
+namespace sinks   = boost::log::sinks;
+namespace src     = boost::log::sources;
+// namespace expr = boost::log::expressions;
+// namespace attrs = boost::log::attributes;
+// namespace keywords = boost::log::keywords;
+
+#define HW_TRACE BOOST_LOG_SEV(lg, logging::trivial::trace)
+#define HW_DEBUG BOOST_LOG_SEV(lg, logging::trivial::debug)
+#define HW_INFO BOOST_LOG_SEV(lg, logging::trivial::info)
+#define HW_WARN BOOST_LOG_SEV(lg, logging::trivial::warning)
+#define HW_ERROR BOOST_LOG_SEV(lg, logging::trivial::error)
+#define HW_FATAL BOOST_LOG_SEV(lg, logging::trivial::fatal)
 
 namespace Server {
 class server_base {
@@ -25,12 +41,12 @@ class server_base {
     virtual ~server_base();
 
   private:
-    lua_State *                                                               L;
-    boost::log::sources::severity_logger<boost::log::trivial::severity_level> lg;
-    bool                                                                      init_config();
-    bool                                                                      init_log();
-    bool                                                                      init_lua();
-    bool                                                                      init_socket();
+    lua_State *L;
+    // logging::sources::severity_logger<logging::trivial::severity_level> lg;
+    bool init_config();
+    bool init_log();
+    bool init_lua();
+    bool init_socket();
 };
 } // namespace Server
 
