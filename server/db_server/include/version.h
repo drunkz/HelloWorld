96104fba0d895ef14c7e209bc3436d20ab0cf6db
drunkz <drunkz@163.com>
2021-06-17 17:36:52 +0800

    sdfg

--- server/db_server/include/version.h
+++ server/db_server/include/version.h
@@ -5,7 +5,7 @@
 #define VERSION_MINOR 0
 #define VERSION_PATCH 1
 #define GIT_BRANCH "master"
-#define GIT_HASH "905a9a8"
-#define BUILD_TIME "20210617162833"
+#define GIT_HASH "46b3d06"
+#define BUILD_TIME "20210617173248"
 
 #endif // VERSION_H_INCLUDED
